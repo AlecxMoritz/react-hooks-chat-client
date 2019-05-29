77cc9620548e2d097bbb5391f75f6ccb28f0f4be
Alecx Moritz <alecxmoritz7@gmail.com>
2019-05-29 12:38:18 -0400

    added router & switched sidebar to bottom bar - stable

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -1,30 +1,34 @@
 import React, { useState } from 'react';
-import { Container, Row, Col } from 'reactstrap'
-import Sidebar from '../UI/Sidebar/Sidebar';
-import ActiveDisplay from './ActiveDisplay/ActiveDisplay';
+import { Container, Row, Col } from 'reactstrap';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import BottomBar from '../UI/BottomBar/BottomBar';
+import ChannelsRoute from './ChannelsRoute/ChannelsRoute';
+import ChannelInfoRoute from './ChannelInfoRoute/ChannelInfoRoute';
+import SearchRoute from './SearchRoute/SearchRoute';
+import SettingsRoute from './SettingsRoute/SettingsRoute';
+import CreateRoute from './CreateRoute/CreateRoute';
+import DashboardRoute from './DashboardRoute/DashboardRoute';
 
 const Main = (props) => {
-    const [ activeItem, setActiveItem ] = useState({})
-    const [ activeType , setActiveType ] = useState('')
- 
-    const setActive = (item, type) => {
-        setActiveItem(item);
-        setActiveType(type);
-        console.log(activeItem);
-        console.log(activeType);
-    }
-
-
     return (
         <Container fluid={ true }>
-            <Row>
-                <Col xs="4">
-                    <Sidebar token={ props.token } logout={ props.logout } setActive={ setActive }/>
-                </Col>
-                <Col xs="8">
-                    <ActiveDisplay item={ activeItem } type={ activeType } token={ props.token } />
-                </Col>
-            </Row>
+            <Router>
+                <Row>
+                    <Col xs="12">
+                        <Switch>
+                            <Route path="/dashboard" exact component={ DashboardRoute }></Route>
+                            <Route path="/channels" exact component={ ChannelsRoute }></Route>
+                            <Route path="/info" exact component={ ChannelInfoRoute }></Route>
+                            <Route path="/search" exact component={ SearchRoute }></Route>
+                            <Route path="/settings" exact component={ SettingsRoute }></Route>
+                            <Route path="/create" exact component={ CreateRoute }></Route>
+                        </Switch>
+                    </Col>
+                </Row>
+                <Row>
+                    <BottomBar token={ props.token } logout={ props.logout } />
+                </Row>
+            </Router>
         </Container>
     )
 }
